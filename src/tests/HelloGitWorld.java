70f9d57125b781ebdc2d8519214cf0a1d41ec358
davutbilgic <db.davutbilgic@gmail.com>
2020-05-06 16:46:12 +0200

    try1

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -4,6 +4,9 @@ public class HelloGitWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello new world");
+
+
+        
         System.out.println("hello hello");
     }
 }
